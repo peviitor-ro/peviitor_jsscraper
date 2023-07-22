490442f0ac481e9dcec3b1db3c27172be0150e15
lalalaurentiu <laurentiumarianbaluta@gmail.com>
2023-07-22 23:54:15 +0300

    adding render page method

---
+++ examples/example_renderPage.js
@@ -0,0 +1,52 @@
+const { Scraper, postApiPeViitor } = require("../index");
+
+const url =
+  "https://careers.celestica.com/search/?createNewAlert=false&q=&locationsearch=Romania&startrow=10";
+
+  const generateJob = (job_title, job_link, city) => ({
+    job_title,
+    job_link,
+    country: "Romania",
+    city,
+  })
+
+const getJobs = async () => {
+    const scraper = new Scraper(url);
+    const soup = await scraper.render_page();
+    const tbody = soup.find("tbody");
+    const trs = tbody.findAll("tr");
+
+    const jobs = [];
+
+    trs.forEach((tr) => {
+        const job_title = tr.find("a").text;
+        const job_link = "https://careers.celestica.com" + tr.find("a").attrs.href;
+        const city = tr.find("span", { class: "jobLocation" }).text.split(",")[0].trim();
+        const job = generateJob(job_title, job_link, city);
+        jobs.push(job);
+    });
+
+    return jobs;
+};
+
+const getParams = () => {
+    const company = "Celestica";
+    const logo = "https://rmkcdn.successfactors.com/bcf7807a/f4737f7e-31d4-4348-963c-8.png";
+    const apikey = process.env.LAURENTIU;
+    const params = {
+        company,
+        logo,
+        apikey,
+    };
+    return params;
+};
+
+const run = async () => {
+    const jobs = await getJobs();
+    const params = getParams();
+    await postApiPeViitor(jobs, params);
+};
+
+run(); // this will be called by our main.js job
+
+module.exports = { getJobs, getParams }; // this is needed for our unit test job
